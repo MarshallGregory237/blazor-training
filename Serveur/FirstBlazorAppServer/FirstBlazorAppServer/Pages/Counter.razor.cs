4d517f1d516cb3c121d1c418f60dd0acdd9f4de8
gregory <gregorysopy@gmail.com>
2024-08-01 11:07:05 +0100

    u

---
+++ Serveur/FirstBlazorAppServer/FirstBlazorAppServer/Pages/Counter.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+
+
+namespace FirstBlazorAppServer.Pages
+{
+    public partial class Counter
+    {
+
+        private int currentCount = 0;
+
+        [Parameter]
+        public int Value { get => currentCount; set => currentCount = value; }
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+    }
+
+}
---
+++ Serveur/FirstBlazorAppServer/FirstBlazorAppServer/Pages/NombreMagic.razor.cs
@@ -0,0 +1,75 @@
+﻿
+
+using Microsoft.AspNetCore.Components;
+
+
+namespace FirstBlazorAppServer.Pages
+{
+    public class NombreMagicBase: ComponentBase
+    {
+
+        protected const int NbLifeMax = 5;
+        
+        protected const int NbMax = 20;
+
+        protected int value;
+
+
+        protected  int NbMagic = 20;
+
+        protected  int NbRemainingLife;
+
+        protected bool? gameWon;
+
+
+        protected override void OnInitialized()
+        {
+            
+            ReinitGame();
+            base.OnInitialized();
+        
+        }
+
+        public void ReinitGame()
+        {
+            var random = new Random();
+
+            // faire un random non negatif qui varie entre 0 et le nombre max
+            NbMagic = random.Next(NbMax);
+
+            NbRemainingLife = NbLifeMax;
+
+            gameWon = null;
+
+            value = 0;
+
+        }
+
+
+
+        protected void TestNumber()
+        {
+
+            if (value == NbMagic)
+            {
+                gameWon = true;
+            }
+
+            else
+            {
+
+                NbRemainingLife--;
+
+                if (NbRemainingLife == 0)
+                {
+                    gameWon = false;
+                }
+
+            }
+
+        }
+
+       
+    }
+
+}
